58fd132b15d78732af1fa9663c2f460f77dc823b
joylin03 <1186714691@qq.com>
2020-04-15 15:03:19 +0800

    add files

---
+++ block_io.py
@@ -0,0 +1,31 @@
+"""
+非阻塞IO演示
+套接字io --> 非阻塞
+"""
+
+from socket import *
+from time import ctime,sleep
+
+
+f = open('net.log','a+')  # 日志
+
+# 创建tcp套接字
+sockfd = socket()
+sockfd.bind(('127.0.0.1',8888))
+sockfd.listen(3)
+
+# 设置套接字非阻塞
+sockfd.setblocking(False)
+
+while True:
+    print("Waiting for connect...")
+    try:
+        connfd,addr = sockfd.accept()  # 阻塞
+        print("Connect from",addr)
+    except BlockingIOError as e:
+        sleep(3)
+        f.write("%s : %s\n"%(ctime(),e))
+        f.flush()
+    else:
+        data = connfd.recv(1024)
+        print(data)
